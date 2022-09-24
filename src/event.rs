b10027055c4d1959e7424a0a37a9932b7e170ed1
Eduard Müller <mail@emuell.net>
2022-09-24 21:29:35 +0200

    add more note event creation helper functions

--- src/event.rs
+++ src/event.rs
@@ -1,7 +1,9 @@
 //! Value and value iters which get emitted by `Rhythms`.
 
-use self::fixed::FixedEventIter;
-use crate::convert::ToFixedEventValue;
+use self::{
+    fixed::{FixedEventIter, ToFixedEventValue},
+    sequence::{EventIterSequence, ToEventIterSequence},
+};
 
 use core::sync::atomic::{AtomicUsize, Ordering};
 use std::fmt::Debug;
@@ -51,6 +53,44 @@ pub fn new_note<Instrument: Into<Option<InstrumentId>>>(
     }
 }
 
+/// Shortcut for creating a new sequence of [`NoteEvent`]:
+/// e.g. a sequence of single notes
+pub fn new_note_sequence<I: Into<Option<InstrumentId>>>(
+    sequence: Vec<(I, u32, f32)>,
+) -> Vec<NoteEvent> {
+    let mut event_sequence = Vec::with_capacity(sequence.len());
+    for (instrument, note, velocity) in sequence {
+        let instrument = instrument.into();
+        event_sequence.push(NoteEvent {
+            instrument,
+            note,
+            velocity,
+        });
+    }
+    event_sequence
+}
+
+/// Shortcut for creating a new sequence of polyphonic [`NoteEvent`]:
+/// e.g. a sequence of chords
+pub fn new_polyphonic_note_sequence<I: Into<Option<InstrumentId>>>(
+    polyphonic_sequence: Vec<Vec<(I, u32, f32)>>,
+) -> Vec<Vec<NoteEvent>> {
+    let mut polyphonic_event_sequence = Vec::with_capacity(polyphonic_sequence.len());
+    for sequence in polyphonic_sequence {
+        let mut event_sequence = Vec::with_capacity(sequence.len());
+        for (instrument, note, velocity) in sequence {
+            let instrument = instrument.into();
+            event_sequence.push(NoteEvent {
+                instrument,
+                note,
+                velocity,
+            });
+        }
+        polyphonic_event_sequence.push(event_sequence);
+    }
+    polyphonic_event_sequence
+}
+
 /// Shortcut for creating a new [`NoteEvent`] [`EventIter`].
 pub fn new_note_event<Instrument: Into<Option<InstrumentId>>>(
     instrument: Instrument,
@@ -58,6 +98,29 @@ pub fn new_note_event<Instrument: Into<Option<InstrumentId>>>(
     velocity: f32,
 ) -> FixedEventIter {
     new_note(instrument, note, velocity).to_event()
+}
+
+/// Shortcut for creating a new sequence of [`NoteEvent`] [`EventIter`].
+pub fn new_note_event_sequence<I: Into<Option<InstrumentId>>>(
+    sequence: Vec<(I, u32, f32)>,
+) -> EventIterSequence {
+    new_note_sequence(sequence).to_event_sequence()
+}
+
+/// Shortcut for creating a single [`EventIter`] from multiple [`NoteEvent`]:
+/// e.g. a chord.
+pub fn new_polyphonic_note_event<I: Into<Option<InstrumentId>>>(
+    polyphonic_events: Vec<(I, u32, f32)>,
+) -> FixedEventIter {
+    new_note_sequence(polyphonic_events).to_event()
+}
+
+/// Shortcut for creating a single [`EventIter`] from multiple [`NoteEvent`]:
+/// e.g. a sequence of chords.
+pub fn new_polyphonic_note_sequence_event<I: Into<Option<InstrumentId>>>(
+    polyphonic_sequence: Vec<Vec<(I, u32, f32)>>,
+) -> EventIterSequence {
+    new_polyphonic_note_sequence(polyphonic_sequence).to_event_sequence()
 }
 
 // -------------------------------------------------------------------------------------------------
