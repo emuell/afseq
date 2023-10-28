9e4bc8493a07c2e1fa10163167bbbaf4b9df0cb3
Eduard Müller <mail@emuell.net>
2023-10-28 20:42:59 +0200

    add missing mod docs for bindings

--- src/bindings.rs
+++ src/bindings.rs
@@ -1,3 +1,5 @@
+//! Script binding modules for the entire crate (currently Lua and Rhai)
+
 use ::mlua::Lua;
 use ::rhai::Engine as RhaiEngine;
 
@@ -6,12 +8,16 @@ pub mod lua;
 
 // ---------------------------------------------------------------------------------------------
 
-/// Create a new rhai engine with preloaded packages and our default configuation
+/// Create a new raw rhai engine, configured for use in our bindings.
+/// To actually use it for scripting, call [register_bindings](rhai::register_bindings) 
+/// on the new engine.
 pub fn new_rhai_engine() -> RhaiEngine {
     rhai::new_engine()
 }
 
-/// Create a new lua engine with preloaded packages and our default configuation
+/// Create a new raw lua engine, configured for use in our bindings.
+/// To actually use it for scripting, call [register_bindings](lua::register_bindings) 
+/// on the new engine.
 pub fn new_lua_engine() -> Lua {
     lua::new_engine()
 }
