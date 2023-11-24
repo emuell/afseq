2bba883bce1574524b5524240574ce1f46eb4ae2
Eduard Müller <mail@emuell.net>
2023-11-24 09:32:50 +0100

    cosmetics

--- assets/examples/demo/synth.lua
+++ assets/examples/demo/synth.lua
@@ -6,6 +6,6 @@ return Emitter {
     note("c-4'm"),
     note("g-3'm7"),
     note("c-4'm"),
-    note("c-4'm"):transpose({0, 0, 3})
+    note("c-4'm", "g4 0.6"):transpose({0, 0, 3})
   ):amplify({0.5, 0.4, 0.5, 0.4})
 }
