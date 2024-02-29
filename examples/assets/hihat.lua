6215ffdd645f83b678998023887a9c14fbfeceae
Eduard Müller <mail@emuell.net>
2024-02-29 13:18:19 +0100

    update examples to use emitter contexts

--- examples/assets/hihat.lua
+++ examples/assets/hihat.lua
@@ -4,13 +4,12 @@ return emitter {
   unit = "1/16",
   offset = 16 * 8,
   pattern = pattern.euclidean(14, 16, 6),
-  emit = function()
-    local note_step = 0;
-    return function()
-      note_step = note_step + 1
-      local volume = 1.0 - (note_step % 4) / 4.0
+  emit = function(initial_context)
+    ---@param context EmitterContext
+    return function(context)
+      local volume = 1.0 - (context.step % 4) / 4.0
       local key = "c6"
-      if note_step % 3 == 0 then
+      if context.step % 3 == 0 then
         key = "c5"
         volume = volume * 0.6
       end
--- examples/assets/kick.lua
+++ examples/assets/kick.lua
@@ -1,11 +1,10 @@
 return emitter {
   unit = "1/16",
   pattern = function()
-    local values, step = table.create({ 0, 6, 10 }), 0
-    return function()
-      local pulse = values:find(step % 16) ~= nil
-      step = step + 1
-      return pulse
+    local pulses = table.create({ 0, 6, 10 })
+    ---@param context EmitterContext
+    return function(context)
+      return pulses:find((context.step - 1) % 16) ~= nil
     end
   end,
   emit = { 60, 60, note { 60, { key = 96, volume = 0.135 } } },
