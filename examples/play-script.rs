4217ee2cb1206ffd72d8be915d22a665901afb67
Eduard Müller <mail@emuell.net>
2024-03-03 22:41:33 +0100

    fixed build errors in play-script example

--- examples/play-script.rs
+++ examples/play-script.rs
@@ -1,7 +1,9 @@
 use std::{
+    cell::RefCell,
     collections::HashSet,
     fs,
     path::{Path, PathBuf},
+    rc::Rc,
     sync::{
         atomic::{AtomicBool, Ordering},
         Arc, RwLock,
@@ -125,14 +127,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         // build final phrase
         let load = |instrument: Option<InstrumentId>, file_name: &str| {
-            bindings::new_rhythm_from_file_with_fallback(beat_time, instrument, file_name)
-                .unwrap_or_else(|err| {
-                    log::warn!("Script '{}' failed to compile: {}", file_name, err);
-                    Rc::new(RefCell::new(BeatTimeRhythm::new(
-                        time_base,
-                        BeatTimeStep::Beats(1.0),
-                    )))
-                })
+            bindings::new_rhythm_from_file(beat_time, instrument, file_name).unwrap_or_else(|err| {
+                log::warn!("Script '{}' failed to compile: {}", file_name, err);
+                Rc::new(RefCell::new(BeatTimeRhythm::new(
+                    beat_time,
+                    BeatTimeStep::Beats(1.0),
+                )))
+            })
         };
         let phrase = Phrase::new(
             beat_time,
