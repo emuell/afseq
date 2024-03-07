1a73ac7f8eee7d33ad3bec0af0f20a89a1e4b9a4
Eduard Müller <mail@emuell.net>
2024-03-07 14:09:04 +0100

    pulse doc and iter impl cosmetics

--- src/pulse.rs
+++ src/pulse.rs
@@ -1,28 +1,33 @@
-//! Pulse within a pattern as used by `Pattern`.
+//! Pulse event within a `Pattern`.
 
 // -------------------------------------------------------------------------------------------------
 
+/// Pulse value in a pattern in range \[0 - 1\] where 0 means don't trigger anything, and 1 means
+/// definitely do trigger something. Values within 0 - 1 maybe trigger, depending on the
+/// pattern/rhythm impl.
 pub type PulseValue = f32;
+/// Pulse step time fraction in a pattern in range \[0 - 1\]. 1 means advance by a full step, 0.5
+/// means advance by a half step, etc.
 pub type PulseStepTime = f64;
 
 // -------------------------------------------------------------------------------------------------
 
-/// Representes single pulse event or a sub division of pulses in patterns.
+/// Represents a single pulse event or a sub division of pulse events in a pattern step.
 ///
-/// When the pattern is played, each pulse uses the duration of a single step as it's base duration,
-/// so a single pulse event will last exactly one step, but a SubDivision pulse vector will convert
-/// the entire steps duration too, so each pulse in the sub division gets fold into the parent step's
-/// timing.
-///  
-/// Using SubDivision thus can use fractions of a rhythms base time step.
+/// When a pattern is played, each pulse or the subdivision use the duration of a single step as
+/// its base duration, so a single pulse event will last exactly one step, but pulses in a sub
+/// division pulse vector will cover the entire step's duration too.
+///
+/// By using pulses with sub divisions, one can create any kind of complex rhythms without
+/// increasing the step resolution.
 #[derive(Clone, Debug, PartialEq, PartialOrd)]
 pub enum Pulse {
     Pulse(PulseValue),
-    SubDivisions(Vec<Pulse>),
+    SubDivision(Vec<Pulse>),
 }
 
 impl Pulse {
-    /// Returns true when the pulse is a subdivision and the subdivision is empty.
+    /// Returns true when the pulse is a sub division and the sub division is empty.
     pub fn is_empty(&self) -> bool {
         self.len() == 0
     }
@@ -31,15 +36,34 @@ impl Pulse {
     pub fn len(&self) -> usize {
         match self {
             Pulse::Pulse(_) => 1,
-            Pulse::SubDivisions(sub) => sub.len(),
+            Pulse::SubDivision(sub_div) => sub_div.len(),
+        }
+    }
+
+    /// Returns a flattened copy of all underlying pulse values.
+    pub fn flattened(&self) -> Vec<PulseIterItem> {
+        let mut values = vec![];
+        self.expand_into(&mut values, 1.0);
+        values
+    }
+
+    fn expand_into(&self, result: &mut Vec<PulseIterItem>, step_time: PulseStepTime) {
+        match self {
+            Pulse::Pulse(value) => result.push((*value, step_time)),
+            Pulse::SubDivision(ref sub_pulses) => {
+                for sub_pulse in sub_pulses {
+                    let sub_step_time = step_time / sub_pulses.len() as f64;
+                    sub_pulse.expand_into(result, sub_step_time);
+                }
+            }
         }
     }
 }
 
 /// Converts a boolean to a pulse.
 impl From<bool> for Pulse {
-    fn from(val: bool) -> Self {
-        Pulse::Pulse(val as u8 as PulseValue)
+    fn from(value: bool) -> Self {
+        Pulse::Pulse(value as u8 as PulseValue)
     }
 }
 
@@ -60,83 +84,67 @@ impl From<f32> for Pulse {
 /// Converts a vector of booleans to a pulse sub division.
 impl From<Vec<bool>> for Pulse {
     fn from(values: Vec<bool>) -> Self {
-        Pulse::SubDivisions(values.into_iter().map(|p| p.into()).collect())
+        Pulse::SubDivision(values.into_iter().map(Pulse::from).collect())
     }
 }
 
 /// Converts a vector of integers to a pulse sub division.
 impl From<Vec<u32>> for Pulse {
     fn from(values: Vec<u32>) -> Self {
-        Pulse::SubDivisions(values.into_iter().map(|p| p.into()).collect())
+        Pulse::SubDivision(values.into_iter().map(Pulse::from).collect())
     }
 }
 
 /// Converts a vector of floats to a pulse sub division.
 impl From<Vec<f32>> for Pulse {
     fn from(values: Vec<f32>) -> Self {
-        Pulse::SubDivisions(values.into_iter().map(Pulse::Pulse).collect())
+        Pulse::SubDivision(values.into_iter().map(Pulse::from).collect())
     }
 }
 
 /// Converts a vector of pulses to a pulse sub division.
 impl From<Vec<Pulse>> for Pulse {
     fn from(values: Vec<Pulse>) -> Self {
-        Pulse::SubDivisions(values)
+        Pulse::SubDivision(values)
     }
 }
 
 // -------------------------------------------------------------------------------------------------
 
+/// Iterator item of PulseIter.
+pub type PulseIterItem = (PulseValue, PulseStepTime);
+
 /// An iterator over a pulse, recursively flattening all subdivisions.
 #[derive(Clone, Debug)]
 pub struct PulseIter {
-    flattened: Vec<(PulseValue, PulseStepTime)>,
+    values: Vec<PulseIterItem>,
     index: usize,
 }
 
 impl PulseIter {
     pub fn new(pulse: Pulse) -> Self {
-        let mut flattened = vec![];
-        Self::flatten(&mut flattened, &pulse, 1.0);
-        Self {
-            flattened,
-            index: 0,
-        }
-    }
-
-    fn flatten(
-        result: &mut Vec<(PulseValue, PulseStepTime)>,
-        current_pulse: &Pulse,
-        step_time: PulseStepTime,
-    ) {
-        match current_pulse {
-            Pulse::Pulse(value) => result.push((*value, step_time)),
-            Pulse::SubDivisions(ref sub_pulses) => {
-                for sub_pulse in sub_pulses {
-                    let sub_step_time = step_time / sub_pulses.len() as f64;
-                    Self::flatten(result, sub_pulse, sub_step_time);
-                }
-            }
-        }
+        let values = pulse.flattened();
+        let index = 0;
+        Self { values, index }
     }
 }
 
 impl Iterator for PulseIter {
-    type Item = (PulseValue, PulseStepTime);
+    type Item = PulseIterItem;
+
     fn next(&mut self) -> Option<Self::Item> {
-        if self.index >= self.flattened.len() {
+        if self.index >= self.values.len() {
             None
         } else {
-            let item = self.flattened[self.index];
+            let item = self.values[self.index];
             self.index += 1;
             Some(item)
         }
     }
 }
 
-/// Converts a pulse to an iterator.
 impl IntoIterator for Pulse {
-    type Item = (PulseValue, PulseStepTime);
+    type Item = PulseIterItem;
     type IntoIter = PulseIter;
 
     fn into_iter(self) -> Self::IntoIter {
