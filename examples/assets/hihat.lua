222ea7210ee84e80cf41ccf11e06f99ad608e5e0
Eduard Müller <mail@emuell.net>
2024-03-12 12:20:08 +0100

    update examples to use some maybe triggers

--- examples/assets/hihat.lua
+++ examples/assets/hihat.lua
@@ -1,31 +1,33 @@
 local pattern = require "pattern"
 
----comment
+---Wrap a lua integer index value into the given length
 ---@param index integer
----@param size integer
+---@param length integer
 ---@return integer
-local function iwrap(index, size)
-  return (index - 1) % size + 1
+math.iwrap = function(index, length)
+  return (index - 1) % length + 1
 end
 
+math.randomseed(0x12345)
+
 return emitter {
-  unit = "1/4",
+  unit = "1/8",
   pattern = function(context)
-    if iwrap(context.step, 8) == 1 then
-      return { 1, 1, 1 }
+    if math.iwrap(context.step, 8) == 1 then
+      return { 0.8, 0.2, 0.9, 0.2 }
     else
       if math.random() > 0.9 then
-        return { 1, 1 }
+        return { 0.8, 0.9 }
       else 
         return { 1 }
       end
     end
   end,
   emit = function(context)
-    local key = "c6"
-    if iwrap(context.step, context.step_count) == 3 then
-      key = "c5"
+    local note = "c6"
+    if math.iwrap(context.step, context.step_count) == 3 then
+      note = "c5 v0.3"
     end
-    return { key = key }
+    return note
   end
 }
--- examples/assets/snare.lua
+++ examples/assets/snare.lua
@@ -1,8 +1,12 @@
 local fun = require "fun"
 
+math.randomseed(0x13ee127)
+
 return emitter {
   unit = "1/16",
-  pattern = fun.cycle { 0, 0, 0, 0, 1, 0, 0, 0 }:take(7 * 8):chain { 0, 0, 0, 1, 0, 0, 1, 0 }:to_table(),
-  -- pattern = pattern.from(0, 1):spread(4) * 7 + { 0, 0, 0, 1 } + { 0, 0, 1, 0 },
-  emit = note({ key = "C5" }):with_volume(1.4),
+  pattern = fun.cycle { 0, 0, 0, 0, 1, 0, 0.075, 0 }:take(7 * 8):chain { 0, 0, 0, 1, 0, 0, 0.5, 0 }:to_table(),
+  emit = function (context) 
+    -- print(context.step, context.step_value, context.step_time, context.step_count)
+    return { key = "C5", volume = (context.step_value == 1) and 1.4 or 0.7}; 
+  end,
 }
