5fbf151f56ec12b3b29ce70e4818eecb34436786
Eduard Müller <mail@emuell.net>
2024-03-20 13:16:22 +0100

    add scale:fit tests

--- examples/assets/bass.lua
+++ examples/assets/bass.lua
@@ -1,11 +1,22 @@
 local fun = require "fun"
 local pattern = require "pattern"
 
--- local scale = scale("c5", {0,1,3,5,7,9,11}).notes
-local scale = scale("c5", "natural minor").notes
+-- local scale = scale("c5", {0,1,3,5,7,9,11})
+local scale = scale("c5", "natural minor")
+
+--[[
+math.randomseed(125231291212)
+
+local rand_note_gen = function(param, state)
+  local min, max = param[1], param[2]
+  return scale:fit(math.random(min, max)) + 12, state
+end
+
+local bassline = fun.wrap(rand_note_gen, {40, 60}):take(8):to_table()
+]]
 
 local bassline = fun.iter({ { 1 }, { 3 }, { 4 }, { 1 }, { 3 }, { 4 }, { 7, -12 } })
-    :map(function(x) return scale[x[1]] + (x[2] or 0) end):to_table()
+    :map(function(x) return scale.notes[x[1]] + (x[2] or 0) end):to_table()
 
 return emitter {
     unit = "1/8",
