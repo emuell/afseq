f0acfe54dc55660603a2f8ac2b50f691e94c6c89
unlessgames <unlessgames@gmail.com>
2024-05-15 16:18:00 +0100

    add differences to tidal

--- types/nerdo/library/cycle.lua
+++ types/nerdo/library/cycle.lua
@@ -13,9 +13,17 @@ Cycle = {}
 
 --- Create a note sequence from a tidal cycle mini notation string.
 ---
---- TODO: describe supported features
+--- [Tidal Cycles Reference](https://tidalcycles.org/docs/reference/mini_notation/)
+--- Everything works the same except for the following differences:
+--- * Operators currently only accept number on the right side (`a3*2` is valid, `a3*<1 2>` is not)
+--- * Polymeters always require specifying the subdivision on the right (`{a b c d}%3`)
+--- * Random event muting always requires a probability (ie `a3?0.5` instead of `a3?`)
+--- * `:` - Sets the instrument or remappable target instead of selecting samples
+--- * `/` - Slow pattern operator is not implemented yet
+--- * `@` - Elongate operator is missing (use explicit `_` steps instead)
+--- * `.` - Shorthand for groups is missing (use `[a b] [c d]` instead)
+--- * The outermost level is treated as a group, stacks and random choices are valid without brackets (`a | b` is parsed as `[a | b]`)
 ---
----[Tidal Cycles Reference](https://tidalcycles.org/docs/reference/mini_notation/)
 ---@param input string
 ---@return Cycle
 ---### examples:
