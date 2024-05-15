305395aaf5d42369d9a3f7bc0e6b0b70853a5c58
unlessgames <unlessgames@gmail.com>
2024-05-15 16:23:07 +0100

    spelling

--- types/nerdo/library/cycle.lua
+++ types/nerdo/library/cycle.lua
@@ -15,13 +15,13 @@ Cycle = {}
 ---
 --- [Tidal Cycles Reference](https://tidalcycles.org/docs/reference/mini_notation/)
 --- Everything works the same except for the following differences:
---- * Operators currently only accept number on the right side (`a3*2` is valid, `a3*<1 2>` is not)
---- * Polymeters always require specifying the subdivision on the right (`{a b c d}%3`)
+--- * Operators currently only accept numbers on the right side (`a3*2` is valid, `a3*<1 2>` is not)
+--- * Polymeters only work by specifying the subdivision on the right (`{a b c d}%3`)
 --- * Random event muting always requires a probability (ie `a3?0.5` instead of `a3?`)
 --- * `:` - Sets the instrument or remappable target instead of selecting samples
 --- * `/` - Slow pattern operator is not implemented yet
 --- * `@` - Elongate operator is missing (use explicit `_` steps instead)
---- * `.` - Shorthand for groups is missing (use `[a b] [c d]` instead)
+--- * `.` - Shorthand for groups is missing (use `[a b] [c d]` instead or `a b . c d`)
 --- * The outermost level is treated as a group, stacks and random choices are valid without brackets (`a | b` is parsed as `[a | b]`)
 ---
 ---@param input string
