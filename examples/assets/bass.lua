0a99d3cd0bfdc78b62072a7aa8bd071f98dcff96
Eduard Müller <mail@emuell.net>
2024-05-22 15:23:14 +0200

    update examples

--- examples/assets/bass.lua
+++ examples/assets/bass.lua
@@ -1,28 +1,12 @@
-local fun = require "fun"
-local pattern = require "pattern"
-
 -- local scale = scale("c5", {0,1,3,5,7,9,11})
 local scale = scale("c5", "natural minor")
-
---[[
-math.randomseed(125231291212)
-
-local rand_note_gen = function(param, state)
-  local min, max = param[1], param[2]
-  return scale:fit(math.random(min, max)) + 12, state
-end
-
-local bassline = fun.wrap(rand_note_gen, {40, 60}):take(8):cycle()
-]]
-
-local bassline = fun.iter({ { 1 }, { 3 }, { 4 }, { 1 }, { 3 }, { 4 }, { 7, -12 } })
-  :map(function(x)
-      return { key = scale.notes[x[1]] + (x[2] or 0), volume = 0.7 }
-    end)
-  :cycle()
 
 return rhythm {
   unit = "1/8",
   pattern = pattern.from({ 1, 0, 1, 0 }, { 0, 1, 0, 0 }, { 1, 0, 1, 0 }, { 0, 1, 0, 1 }),
-  emit = bassline
+  emit = pattern.generate(function(value)
+      return { key = scale.notes[value[1]] + (value[2] or 0), volume = 0.7 }
+    end,
+    { 1 }, { 3 }, { 4 }, { 1 }, { 3 }, { 4 }, { 7, -12 }
+  )
 }
--- examples/assets/snare.lua
+++ examples/assets/snare.lua
@@ -1,12 +1,9 @@
-local fun = require "fun"
-
 math.randomseed(0x13ee127)
 
 return rhythm {
   unit = "1/16",
-  pattern = fun.cycle { 0, 0, 0, 0, 1, 0, 0.075, 0 }:take(7 * 8):chain { 0, 0, 0, 1, 0, 0, 0.5, 0 }:to_table(),
+  pattern = pattern.from { 0, 0, 0, 0, 1, 0, 0.075, 0 } * 7 + { 0, 0, 0, 1, 0, 0, 0.5, 0 },
   emit = function(context)
-    -- print(context.step, context.step_value, context.step_time, context.step)
     return { key = "C5", volume = (context.pulse_value == 1) and 0.8 or 0.5 }
   end,
 }
