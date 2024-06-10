704d5db57e010d5373444cc7108188ab6874adc0
Eduard Müller <mail@emuell.net>
2024-06-10 21:42:56 +0200

    allow mapping cycle values to custom note events via a static map

--- types/nerdo/library/cycle.lua
+++ types/nerdo/library/cycle.lua
@@ -11,6 +11,29 @@ local Cycle = {}
 
 ----------------------------------------------------------------------------------------------------
 
+---Map names in in the cycle to custom note events.
+---
+---By default, strings in cycles are interpreted as notes, and integer values as MIDI note
+---values. Custom identifiers such as "bd" are undefined and will result into a rest, when
+---they are not mapped explicitely.
+---
+---Chords such as "c4'major" are not (yet) supported as values.
+---@param map { [string]: NoteValue}
+---@return Cycle
+---### examples:
+---```lua
+---cycle("bd [bd sn]"):map({
+---  bd = "c4",
+---  sn = "e4 #1 v0.2"
+---})
+---cycle("bd:1 <bd:5, bd:7>"):map({
+---  bd = { key = "c4", volume = 0.5 }, -- instrument #1,5,7 will be set as specified
+---})
+---```
+function Cycle:map(map) end
+
+----------------------------------------------------------------------------------------------------
+
 --- Create a note sequence from a Tidal Cycles mini-notation string.
 ---
 --- `cycle` accepts a mini-notation as used by Tidal Cycles, with the following differences:
@@ -23,7 +46,7 @@ local Cycle = {}
 ---@return Cycle
 ---### examples:
 --- ```lua
------A chord sequence 
+-----A chord sequence
 ---cycle("[c4, e4, g4] [e4, g4, b4] [g4, b4, d5] [b4, d5, f#5]")
 -----Arpegio pattern with variations
 ---cycle("<c4 e4 g4> <e4 g4> <g4 b4 d5> <b4 f5>")
@@ -31,5 +54,7 @@ local Cycle = {}
 ---cycle("c4(3,8) e4(5,8) g4(7,8)")
 -----Polyrhythm
 ---cycle("{c4 e4 g4 b4}%2, {f4 d4 a4}%4")
+-----Custom name mappings
+---cycle("bd(3,8)"):map({ bd = "c4 #1" })
 --- ```
 function cycle(input) end
