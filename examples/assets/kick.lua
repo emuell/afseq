6a80b1c3f5393d271f1b2eb490ef3a8f7cd8d33a
Eduard Müller <mail@emuell.net>
2024-06-12 13:44:55 +0200

    add cycle map tests to examples

--- examples/assets/kick.lua
+++ examples/assets/kick.lua
@@ -1,11 +1,12 @@
 return rhythm {
-  unit = "1/16",
-  pattern = function()
-    local pulses = table.create({ 0, 6, 10 })
-    ---@param context PatternContext
-    return function(context)
-      return pulses:find((context.pulse_step - 1) % 16) ~= nil
+  unit = "1/4",
+  resolution = 16,
+  emit = cycle("bd [~ bd] ~ ~ bd [~ bd] _ ~ bd [~ bd] ~ ~ bd [~ bd] [_ bd2] [~ bd _ ~]"):map(
+    function(context, value)
+      -- print(context.channel, context.step, context.step_length, "->", value)
+      if value == "bd" or value == "bd2" then
+        return { key = 48, volume = value == "bd" and 0.9 or 0.5 }
+      end
     end
-  end,
-  emit = { 60, 60, note { 60, { key = 96, volume = 0.135 } } },
+  )
 }
