fe6e26f751dad2e4818ec7b552fe37c83a5cc5e8
Eduard Müller <mail@emuell.net>
2024-07-11 10:46:13 +0200

    remove with_ prefix from mappings and added new interval and transpose mappings

--- examples/assets/bass.lua
+++ examples/assets/bass.lua
@@ -1,18 +1,15 @@
 -- local scale = scale("c5", {0,1,3,5,7,9,11})
-local scale = scale("c5", "natural minor")
 
 return rhythm {
   unit = "1/8",
   pattern = pattern.from({ 1, 0.5, 1, 0 }, { 0, 1, 0, 0 }, { 1, 0, 1, 0 }, { 0, 1, 0, 1 }),
-  gate = function (context)
+  gate = function(context)
     return context.pulse_value == 1.0
   end,
-  emit = pattern.from(1, 3, 4, 1, 3, 4, -7):map(function(index, value)
-    if value < 0 then
-      return { key = scale.notes[-value] - 12, volume = 0.7 }
-    else
-      return { key = scale.notes[value], volume = 0.7 }
-    end
-  end
+  emit = cycle("<1 3 4 1 3 4 7>"):map(
+    mappings.combine(
+      mappings.intervals(scale("c5", "natural minor")),
+      mappings.transpose({ 0, 0, 0, 0, 0, 0, -12 })
+    )
   )
 }
