458fe28d6218cc18917b3e27585d7299671aa08a
Eduard Müller <mail@emuell.net>
2024-08-29 09:46:21 +0200

    add input parameter definition

---
+++ src/input.rs
@@ -0,0 +1,6 @@
+//! Defines optional input parameter handling for a `Rhythm` in order to create dynamic template rhythms.
+
+// -------------------------------------------------------------------------------------------------
+
+pub mod parameter;
+pub use parameter::{Parameter, ParameterMap, ParameterType};
