7ecce935007f8120b3085668d709e78f17704a00
Eduard Müller <mail@emuell.net>
2025-02-13 16:36:40 +0100

    updated example to test cycle rhythms

--- examples/assets/kick.lua
+++ examples/assets/kick.lua
@@ -1,12 +1,4 @@
-return rhythm {
-  unit = "1/4",
-  resolution = 16,
-  emit = cycle("bd [~ bd] ~ ~ bd [~ bd] _ ~ bd [~ bd] ~ ~ bd [~ bd] [_ bd2] [~ bd _ ~]"):map(
-    function(context, value)
-      -- print(context.channel, context.step, context.step_length, "->", value)
-      if value == "bd" or value == "bd2" then
-        return { key = 48, volume = value == "bd" and 0.9 or 0.5 }
-      end
-    end
-  )
+return cycle("[bd [~ bd] ~ ~ bd [~ bd] _ ~ bd [~ bd] ~ ~ bd [~ bd] [_ bd2] [~ bd _ ~]]/4"):map {
+  bd = "c4 v.9",
+  bd2 = "c4 v.5"
 }
